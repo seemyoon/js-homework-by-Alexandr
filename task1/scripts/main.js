145616b927ed9ab36884b12f4f693cd7b6457e12
Alexandr Semenec <alexandrsemenec01@gmail.com>
2024-04-29 23:06:27 +0300

    the task was done

---
+++ task1/scripts/main.js
@@ -0,0 +1,57 @@
+// Task 1: - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14,
+// 2.7, 16, true, false. Вивести кожну змінну за допомогою: console.log
+const firstVariable = 'hello';
+const secondVariable = 'owu';
+const thirdVariable = 'com';
+const fourthVariable = 'ua';
+const fifthVariable = 1;
+const sixthVariable = 10;
+const seventhVariable = -999;
+const eighthVariable = 123;
+const ninthVariable = 3.14;
+const tenthVariable = 2.7;
+const eleventhVariable = 16;
+const twelfthVariable = true;
+const thirteenthVariable = false;
+console.log(firstVariable)
+console.log(secondVariable)
+console.log(thirdVariable)
+console.log(fourthVariable)
+console.log(fifthVariable)
+console.log(sixthVariable)
+console.log(seventhVariable)
+console.log(eighthVariable)
+console.log(ninthVariable)
+console.log(tenthVariable)
+console.log(eleventhVariable)
+console.log(twelfthVariable)
+console.log(thirteenthVariable)
+// Task 2: - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну
+// person (Не об'єкт, просто за допомоги конкатенації)
+const firstName = `Alexandr`;
+const middleName = `Semenets`;
+const lastName = `Mykolayovich`;
+// const person = `${firstName} ${middleName} ${lastName}`;
+// Task 3: - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль. let a = 100;
+// let b = '100'; let c = true;
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+// Task 4: Додаткове для тих хто цікавився prompt`oм - За допомогою 3х різних prompt() отримати 3 слова які
+// являються вашими Імям, По-Батькові та роками. та вивести в консоль
+const firstName2 = prompt();
+const lastName2 = prompt();
+const year = prompt();
+console.log(firstName2)
+console.log(lastName2)
+console.log(year)
+//-----------------Або так, мені так більше подобається :) (одна змінна через шаблоні літерали приймає всі
+// значення)---------
+// const firstName2 = prompt();
+// const lastName2 = prompt();
+// const year = prompt();
+// const person2 = `Your name: ${firstName2}, last name: ${lastName2}, year: ${year}`;
+// console.log(person2)
